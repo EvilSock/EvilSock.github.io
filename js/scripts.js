0fda8082266a42f61e79653a04c53b36d16911fa
Paul Rossouw <paulrossouw11@gmail.com>
2021-02-24 13:54:14 +0200

    inittial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var myCarousel = document.querySelector('#carouselExampleControls')
+
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 3000,
+    wrap: true,
+    ride:  true
+  })
+
+$(document).on("mousewheel",()=>{
+  if ($(document).scrollTop() <= 20 && $(window).width() >= 995){
+		console.log("bruh")
+		$(".color").css({
+			"background-color":"rgba(235, 235, 235,0)"
+	});
+	} else {
+		$(".color").css({
+			"background-color":"#ebebeb"
+	});
+	}
+});
+
